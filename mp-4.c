549f49ebb075f6390b91b1992b1bc1dedb882bbf
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2005-10-21 16:18:27 +0000

    Deleted dead code.

--- mp-4.c
+++ mp-4.c
@@ -2,7 +2,7 @@
 
     Minimum Profit - Programmer Text Editor
 
-    Copyright (C) 1991-2004 Angel Ortega <angel@triptico.com>
+    Copyright (C) 1991-2005 Angel Ortega <angel@triptico.com>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
@@ -29,225 +29,13 @@
 #include "mpdm.h"
 #include "mpsl.h"
 
-#include "mp_core.h"
-
 /*******************
 	Data
 ********************/
 
-mpdm_t t;
-
 /*******************
 	Code
 ********************/
-
-#if 0
-void dump(void)
-{
-	struct mp_txt * txt;
-/*	mpdm_t v;
-	int n, m;
-	char * ptr;
-
-	for(n=0;n < txt->size;n++)
-	{
-		v=mpdm_aget(txt, n);
-		ptr=(char *)v->data;
-
-		printf("[%d]", v->ref);
-
-		for(m=0;m < v->size;m++)
-		{
-			if(m == x && n == y)
-				printf("_");
-
-			if(ptr[m] == '\0')
-			{
-				printf(">");
-				break;
-			}
-
-			printf("%c", ptr[m]);
-		}
-
-			if(m == x && n == y)
-				printf("_");
-
-		printf("\n");
-	}
-*/
-/*
-	{
-		int x,y;
-		mpdm_t lines;
-		mpdm_t line;
-
-		x=mpdm_ival(MPDM_SGET(cdata, "txt.x"));
-		y=mpdm_ival(MPDM_SGET(cdata, "txt.y"));
-		lines=MPDM_SGET(cdata, "txt.lines");
-		line=mpdm_aget(lines, y);
-
-		printf("[%c]\n", ((char *)line->data)[x]);
-	}
-*/
-	mpdm_sweep(0);
-	mpdm_dump(t);
-
-	txt=t->data;
-	printf("[%d, %d]\n", txt->x, txt->y);
-	mpdm_dump(txt->lines);
-
-	getchar();
-}
-
-
-void mp_4_c(void)
-{
-/*	mpdm_dump(mpdm_root(), 0); */
-
-	t=mpdm_ref(mp_new());
-	mp_load_file(t, "config.h");
-
-	dump();
-
-	mp_move_right(t);
-	dump();
-	mp_move_right(t);
-	dump();
-
-	mp_move_eol(t);
-	dump();
-
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-
-	mp_move_down(t);
-	dump();
-	mp_move_down(t);
-	dump();
-
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-
-	mp_move_bol(t);
-	dump();
-
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-	mp_move_left(t);
-	dump();
-
-	mp_move_right(t);
-	dump();
-	mp_move_right(t);
-	dump();
-	mp_move_right(t);
-	dump();
-	mp_move_right(t);
-	dump();
-
-	mp_move_eol(t);
-	dump();
-	mp_move_right(t);
-	dump();
-	mp_move_right(t);
-	dump();
-
-	mp_move_up(t);
-	dump();
-
-	mp_insert_line(t);
-	dump();
-
-	mp_insert(t, MPDM_LS(L"uah!"));
-	dump();
-	mp_insert(t, MPDM_LS(L"UAH?"));
-	dump();
-
-	{
-		mpdm_t w=MPDM_A(0);
-		mpdm_aset(w, MPDM_LS(L"000"), 0);
-		mpdm_aset(w, MPDM_LS(L"111"), 1);
-		mpdm_aset(w, MPDM_LS(L"222"), 2);
-		mpdm_aset(w, MPDM_LS(L"333"), 3);
-
-		mp_insert(t, w);
-		dump();
-	}
-
-	mp_move_eol(t);
-	mp_insert(t, MPDM_LS(L"MUAHAHAHA!!!"));
-	dump();
-
-	mp_move_bol(t);
-	mp_move_right(t);
-	mp_move_right(t);
-	mp_move_right(t);
-	mp_delete(t);
-	dump();
-
-	mp_move_eol(t);
-	mp_delete(t);
-	dump();
-
-	mp_move_eol(t);
-	mp_delete(t);
-	dump();
-
-	mpdm_sweep(-1);
-	dump();
-
-#ifdef QQ
-	mp_move_eol(txt, &x, &y);
-
-	for(n=0;n < 8;n++)
-	{
-		dump();
-		mp_move_left(txt, &x, &y);
-	}
-
-	dump();
-	mpdm_copy(txt);
-	mp_insert_char(txt, &x, &y, '-');
-	dump();
-
-	mpdm_copy(txt);
-	mp_delete_char(txt, &x, &y);
-	dump();
-
-	mpdm_copy(txt);
-	mp_insert_char(txt, &x, &y, '\n');
-	dump();
-
-	mp_move_eol(txt, &x, &y);
-	dump();
-	mp_insert_char(txt, &x, &y, '2');
-	dump();
-
-	mp_delete_char(txt, &x, &y);
-
-	{
-		int n;
-
-		for(n=0;n < 25;n++)
-			dump();
-	}
-#endif
-
-}
-#endif /* #if 0 */
 
 void mp_4_mpsl(void)
 {
@@ -260,17 +48,11 @@ void mp_4_mpsl(void)
 
 int main(void)
 {
-	int n;
-
-/*	mp_startup();*/
-
 	mpdm_startup();
 
 	mp_4_mpsl();
 
 	mpdm_shutdown();
-
-/*	mp_shutdown();*/
 
 	return(0);
 }
