3bd214a67782d28ffdb656290165a586f80e1d92
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2006-09-25 12:12:13 +0000

    Win32's form() is much wider.

--- mpv_win32.c
+++ mpv_win32.c
@@ -1327,7 +1327,7 @@ static mpdm_t w32drv_form(mpdm_t a)
 	lpdt->style = WS_POPUP | WS_BORDER | WS_SYSMENU | DS_MODALFRAME | WS_CAPTION;
 	lpdt->cdit = (2 * mpdm_size(form_args)) + 2;
 	lpdt->x  = 20;  lpdt->y  = 20;
-	lpdt->cx = 160; /* lpdt->cy = 30 + mpdm_size(form_args) * il; */
+	lpdt->cx = 260;
 
 	lpw = (LPWORD)(lpdt + 1);
 	*lpw++ = 0;	/* No menu */
@@ -1344,7 +1344,7 @@ static mpdm_t w32drv_form(mpdm_t a)
 
 		/* label */
 		lpw = build_control(lpw, 10, 5 + p * il,
-			40, 20, 100 + (n * 2), 0x0082,
+			50, 20, 100 + (n * 2), 0x0082,
 			WS_CHILD | WS_VISIBLE | SS_RIGHT);
 
 		type = mpdm_string(mpdm_hget_s(w, L"type"));
@@ -1379,8 +1379,8 @@ static mpdm_t w32drv_form(mpdm_t a)
 			inc = 5;
 		}
 
-		lpw = build_control(lpw, 55, 5 + p * il,
-			100, (inc * il) - 5, 101 + (n * 2), class, style);
+		lpw = build_control(lpw, 65, 5 + p * il,
+			190, (inc * il) - 5, 101 + (n * 2), class, style);
 
 		/* next position */
 		p += inc;
@@ -1390,11 +1390,11 @@ static mpdm_t w32drv_form(mpdm_t a)
 	lpdt->cy = 30 + p * il;
 
 	/* OK */
-	lpw = build_control(lpw, 70, 10 + p * il, 40, 15, IDOK,
+	lpw = build_control(lpw, 170, 10 + p * il, 40, 15, IDOK,
 			0x0080, WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON | WS_TABSTOP);
 
 	/* Cancel */
-	lpw = build_control(lpw, 115, 10 + p * il, 40, 15, IDCANCEL,
+	lpw = build_control(lpw, 215, 10 + p * il, 40, 15, IDCANCEL,
 			0x0080, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | WS_TABSTOP);
 
 	GlobalUnlock(hgbl);
