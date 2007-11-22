216c6723e785c06e6641a0d47796d6ba23469314
Angel Ortega <angel@venexma.es>
2007-11-22 14:54:24 +0100

    Fix wget_wch() detection (Closes: #1247).

--- config.sh
+++ config.sh
@@ -191,7 +191,8 @@ if [ "$WITHOUT_CURSES" != "1" ] ; then
 	# test now for wget_wch() existence
 	echo -n "Testing for wget_wch()... "
 
-	echo "#include <curses.h>" > .tmp.c
+	echo "#include <wchar.h>" > .tmp.c
+	echo "#include <curses.h>" >> .tmp.c
 	echo "int main(void) { wchar_t c[2]; initscr(); wget_wch(stdscr, c); endwin(); return 0; }" >> .tmp.c
 
 	$CC $TMP_CFLAGS .tmp.c $TMP_LDFLAGS -o .tmp.o 2>> .config.log
