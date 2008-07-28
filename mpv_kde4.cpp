de574e5619964b68d6df48c4592eda151c172264
Angel Ortega <angel@venexma.es>
2008-07-28 12:58:53 +0200

    Copied kde4_drv_shutdown() from gtk_drv_shutdown().

--- mpv_kde4.cpp
+++ mpv_kde4.cpp
@@ -169,6 +169,13 @@ static mpdm_t kde4_drv_main_loop(mpdm_t a)
 
 static mpdm_t kde4_drv_shutdown(mpdm_t a)
 {
+	mpdm_t v;
+
+	if ((v = mpdm_hget_s(mp, L"exit_message")) != NULL) {
+		mpdm_write_wcs(stdout, mpdm_string(v));
+		printf("\n");
+	}
+
 	return NULL;
 }
 
