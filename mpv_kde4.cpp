3c2b1e1fbf1d5c3121bcb103b8e4d88cda45c381
Angel Ortega <angel@triptico.com>
2008-08-11 12:09:07 +0200

    Added a lock to ignore tabbar signals while rebuilding.

--- mpv_kde4.cpp
+++ mpv_kde4.cpp
@@ -304,6 +304,7 @@ static void draw_status(void)
 	statusbar->changeItem(str_to_qstring(mp_build_status_line()), 0);
 }
 
+static int ignore_tab_clicks = 0;
 
 static void draw_filetabs(void)
 {
@@ -316,6 +317,8 @@ static void draw_filetabs(void)
 	/* is the list different from the previous one? */
 	if (mpdm_cmp(names, last) != 0) {
 
+		ignore_tab_clicks = 1;
+
 		while (file_tabs->count())
 			file_tabs->removeTab(0);
 
@@ -326,6 +329,8 @@ static void draw_filetabs(void)
 		/* store for the next time */
 		mpdm_unref(last);
 		last = mpdm_ref(names);
+
+		ignore_tab_clicks = 0;
 	}
 
 	/* set the active one */
@@ -480,7 +485,7 @@ void MPArea::from_scrollbar(int value)
 
 void MPArea::from_filetabs(int value)
 {
-	if (value >= 0) {
+	if (!ignore_tab_clicks && value >= 0) {
 		/* sets the active one */
 		mpdm_hset_s(mp, L"active_i", MPDM_I(value));
 		area->update();
